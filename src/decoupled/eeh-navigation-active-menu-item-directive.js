8e5cbc426d74a9eab1d68834434c99c803b55e2c
Ethan Hann <ethanhann@gmail.com>
2015-05-08 15:04:08 -0400

    Depend on state change event rather than watching $location

--- src/decoupled/eeh-navigation-active-menu-item-directive.js
+++ src/decoupled/eeh-navigation-active-menu-item-directive.js
@@ -1,23 +1,22 @@
 'use strict';
 
-var ActiveParentMenuItemDirective = function ($location, $state) {
+var ActiveParentMenuItemDirective = function ($state) {
     return {
         restrict: 'A',
         scope: {
             menuItem: '=eehNavigationActiveMenuItem'
         },
         link: function (scope, element) {
-            var activeClass = 'active';
-            scope.$watch(function () {
-                return $location.url();
-            }, function () {
+            var checkIsActive = function () {
                 if (scope.menuItem.hasChildren()) {
                     var isActive = scope.menuItem.children().filter(function (childMenuItem) {
                             return angular.isDefined(childMenuItem.state) && $state.includes(childMenuItem.state);
                         }).length > 0;
-                    element.toggleClass(activeClass, isActive);
+                    element.toggleClass('active', isActive);
                 }
-            });
+            };
+            scope.$on('$stateChangeSuccess', checkIsActive);
+            checkIsActive();
         }
     };
 };
