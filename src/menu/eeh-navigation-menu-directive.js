f00d193d8995eea1d6b9754ecfc34863bdcf5a28
Ethan Hann <ethanhann@gmail.com>
2015-06-16 20:33:35 -0400

    Refactor "list" to "menu"

--- src/menu/eeh-navigation-menu-directive.js
+++ src/menu/eeh-navigation-menu-directive.js
@@ -2,18 +2,21 @@
 
 /**
  * @ngdoc directive
- * @name eeh-navigation-list
+ * @name eeh-navigation-menu
  * @restrict AE
  *
  * @description
  * This directive allows for the creation of a framework-agnostic menu.
+ * It is possible to add an a directive that adds behavior to, or otherwise manipulates, this directive.
+ * Third party jQuery menu plugins, such as Superfish and MetisMenu, can easily be used to style and add behavior to the
+ * menu that this directive generates.
  *
  * @param {string} menuName Sets the name of the menu that the directive will render.
  */
 var ListDirective = function (eehNavigation) {
     return {
         restrict: 'AE',
-        templateUrl: 'template/eeh-navigation/list/eeh-navigation-list.html',
+        templateUrl: 'template/eeh-navigation/menu/eeh-navigation-menu.html',
         scope: {
             menuName: '='
         },
