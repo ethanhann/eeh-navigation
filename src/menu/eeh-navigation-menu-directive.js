672f23fc52a4ed61cbd40cb152487c7936b682ab
Ethan Hann <ethanhann@gmail.com>
2015-06-18 15:03:46 -0400

    Update menu doc

--- src/menu/eeh-navigation-menu-directive.js
+++ src/menu/eeh-navigation-menu-directive.js
@@ -7,9 +7,9 @@
  *
  * @description
  * This directive allows for the creation of a framework-agnostic menu.
- * It is possible to add a directive that adds behavior to, or otherwise manipulates, this directive.
+ * It is encouraged for users to create their own directive that adds behavior to, or otherwise manipulates, this directive.
  * Third party jQuery menu plugins, such as Superfish and MetisMenu, can easily be used to style and add behavior to the
- * menu that this directive generates.
+ * bare-bones menu that this directive generates.
  *
  * @param {string} menuName Sets the name of the menu that the directive will render.
  */
