2f92e608a0d439c51eac221f43b034d128d6238b
Ethan Hann <ethanhann@gmail.com>
2015-07-07 02:39:42 -0400

    Update docs post release

--- docs/src/app/docs/docs-controller.js
+++ docs/src/app/docs/docs-controller.js
@@ -5,6 +5,7 @@
 
 angular.module('docs').controller('DocsController', function ($scope) {
     $scope.navbarBrand = {
+        src: 'app/main/logo-alt.png',
         text: 'eeh-navigation',
         state: 'home'
     };
